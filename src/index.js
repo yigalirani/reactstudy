866b37ca2b7eb8d12bd36eaa2bfe38353f64d78a
Yigal Irani <yigal@symbolclick.com>
2019-06-15 20:01:14 -0400

    midwork adding router

--- src/index.js
+++ src/index.js
@@ -1,98 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import {Mylabel,Lister} from './lister.js'
-const a=size=>Array(size).fill(null)
-const Square = props=> 
-  <button 
-    className="square" 
-    onClick={() => props.onClick()}
-  >
-    {props.value}
-  </button>
-
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square 
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
-    />;
-  }
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: a(9),
-      next:'X'
-    };
-  }
-  handleClick(i) {
-    const squares = this.state.squares.slice(); //is slice just copies the data? look it up
-    squares[i] = this.state.next;
-    this.setState({squares: squares,next:this.state.next==='X'?'O':'X'});
-  }  
-  renderRow(start){
-    return <div className="board-row">{a(3).map((x,pos)=>this.renderSquare(pos+start))}</div> 
-  }
-  render() {
-    return <div>{this.render2()}{this.render2()}</div>
-  } 
-  render2() {
-    const status = 'Next player: '+this.state.next;
-    const winner=calculateWinner(this.state.squares)
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="status">winnder:{winner}</div>
-        {this.renderRow(0)}
-        {this.renderRow(3)}
-        {this.renderRow(6)}
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
+import {Mylabel,Lister,AppRouter} from './lister.js'
 
 // ========================================
 
 ReactDOM.render(
   <div>
-    <Lister />
+    <AppRouter />
   </div>
   ,
   document.getElementById('root')
 );
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
--- src/lister.js
+++ src/lister.js
@@ -1,4 +1,42 @@
 import React, { Component }  from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+export function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Lister} />
+      </div>
+    </Router>
+  );
+}
 export let Mylabel=(props)=>(<div>{props.text}</div>)
 class Input extends React.Component{
 	constructor(props){
@@ -19,7 +57,7 @@ class Input extends React.Component{
 	
 
 		return <form onSubmit={this.onSubmit} >{this.state.num_changes}
-						<input type="text" value={this.state.text} onChange={this.onChange}/>
+						<input type="text" value={this.state.text.replace('xx',' ')} onChange={this.onChange}/>
 				</form>
 	}
 }
@@ -45,7 +83,8 @@ export class  Lister extends React.Component {
 
 	
 	render(){
-		return <div><Input onSubmit={this.onSubmit} text={this.state.text}/><ol>{
+		return <div>
+		<Input onSubmit={this.onSubmit} text={this.state.text}/><ol>{
 			this.state.list.map(x=><li key={x.id}> {x.tx}</li>)
 		}</ol></div>
 	}
