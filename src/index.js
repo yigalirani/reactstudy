21c6d7c0e9106bd2825cd5a3b097fe711e064952
Yigal Irani <yigal@symbolclick.com>
2019-06-29 18:59:15 -0400

    reverted some auto format changes, more progress with my router

--- src/index.js
+++ src/index.js
@@ -1,107 +1,105 @@
-import React, { useState, useEffect } from 'react';
+import React,{ useState,useEffect } from 'react';
 import ReactDOM from 'react-dom';
-
 import './index.css';
-import { Mylabel, Lister } from './lister.js'
+import { Mylabel,Lister } from './lister.js'
 import { Game } from './game.js'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import { BrowserRouter as Router,Route,Link } from "react-router-dom";
 
 function Tiker() {
-  var [count, setCount] = useState(0);
-  function tick() {
-    console.log('tick', count)
-    setCount(count => count + 1)
-  }
-  useEffect(() => {
-    var timerID = setInterval(_ =>
-      setCount(count => count + 1)//setCount(count+1) wont work
-      , 1000);
-    console.log('setinterval', count)
+    var [count,setCount]=useState(0);
 
-    return function cleanup() {
-      clearInterval(timerID);
-      console.log('clearinterval')
-    };
-  }, [count]);
-  return <div>
+    function tick() {
+        console.log('tick',count)
+        setCount(count=>count+1)
+    }
+    useEffect(()=>{
+        var timerID=setInterval(_=>setCount(count=>count+1) //setCount(count+1) wont work
+            ,1000);
+        console.log('setinterval',count)
+        return function cleanup() {
+            clearInterval(timerID);
+            console.log('clearinterval')
+        };
+    },[count]);
+    return <div>
     this is ticker
       <button onClick={() =>
-      setCount(count + 1)//setCount(count+1) does work
+      setCount(count+1)//setCount(count+1) does work
     }>up </button>
     {count}
   </div>
 }
-
 class ClassTiker extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      count: 0
-    };
-  }
-  componentDidMount() {
-    setInterval(_ => this.setState({ count: this.state.count + 1 }), 1000)
-    console.log('ClassTiker:componentDidMount')
-  }
-
-  render() {
-    return <span>this is classticker{this.state.count}</span>
-  }
+    constructor() {
+        super()
+        this.state={
+            count: 0
+        };
+    }
+    componentDidMount() {
+        setInterval(_=>this.setState({ count: this.state.count+1 }),1000)
+        console.log('ClassTiker:componentDidMount')
+    }
+    render() {
+        return <span>this is classticker{this.state.count}</span>
+    }
 }
-var MyDiv = ({ children }) => <div className='high'> {children} {children}</div>
+var MyDiv=({ children })=><div className='high'> {children} {children}</div>
+
 function Index() {
-  return <MyDiv><h2>Home</h2>
+    return <MyDiv><h2>Home</h2>
     <Tiker />
   </MyDiv>
 }
 
 function About() {
-  return <h2>About</h2>;
+    return <h2>About</h2>;
 }
 
 function Users() {
-  return <h2>Users</h2>;
+    return <h2>Users</h2>;
 }
-function Example() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
 
-  return (
-    <div>
+function Example() {
+    // Declare a new state variable,which we'll call "count"
+    const [count,setCount]=useState(0);
+    return (<div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
+      <button onClick={()=>setCount(count+1)}>
         up
       </button>
-      <button onClick={() => setCount(count - 1)}>
+      <button onClick={()=>setCount(count-1)}>
         down
       </button>
-    </div>
-  );
+    </div>);
 }
-function eq(a, b) {
-  return a.toLowerCase() == b.toLowerCase()
+
+function eq(a,b) {
+    return a.toLowerCase()==b.toLowerCase()
 }
-function MyRouter({ routes, home }) {
-  var hash = window.location.hash.substr(1)
-  var path = hash.split('/')
-  function find_component() {
-    for (const [name, component] of Object.entries(routes)) {
-      if (eq(name, path[0])) {
-        console.log(name)
-        return new component()
-      }
+
+function MyRouter({ routes,home }) {
+    var hash=window.location.hash.substr(1)
+    var path=hash.split('/')
+
+    function Choose() {
+        for(const [name,Component] of Object.entries(routes)) {
+            if(eq(name,path[0])) {
+                console.log(name)
+                return <Component/>
+            }
+        }
+        var Home=home
+        return <Home/>
     }
-    return home()
-  }
-
-  return find_component()
+    function Menu(){
+      return <div>{Object.keys(routes).map(x=><a href={'#'+x}>{x}</a>)}</div>
+    }
+    return <div><Menu/><Choose/></div>
 }
-
-  class AppRouter extends React.Component {
-
+class AppRouter extends React.Component {
     render() {
-      return <Router>eee{this.props.r.map(x => x)}
+        return <Router>eee{this.props.r.map(x=>x)}
         <ul>
           <li>
             <Link to="/">Home</Link><Example />
@@ -119,11 +117,7 @@ function MyRouter({ routes, home }) {
         <Route path="/lister/" component={Lister} />
       </Router>
     }
-  }
-  //{Index,Game,Lister }
-  ReactDOM.render(
-    <MyRouter home={Index} routes={{ Index, Game, Lister }} />
-    ,
-    document.getElementById('root')
-  );
+}
+//{Index,Game,Lister }
+ReactDOM.render(<MyRouter home={Index} routes={{ Index,Game,Lister }} />,document.getElementById('root'));
 //<AppRouter r={['Index','Game','Lister' ]}/>
