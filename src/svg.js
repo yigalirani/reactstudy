d1a6f7147e1680c91c80a453bd9c604ab46777cf
Yigal Irani <yigal@symbolclick.com>
2020-01-10 10:37:09 -0500

    added more svg

--- src/svg.js
+++ src/svg.js
@@ -12,15 +12,19 @@ export function Svgdemo(){
 	useEffect(_=>setint(),[])
 	return <svg version="1.1"
      baseProfile="full"
-     width="300" height="200"
-     xmlns="http://www.w3.org/2000/svg">
+     width="1000" height="1000"
+     xmlns="http://www.w3.org/2000/svg"
+     >
 
   <rect width="100%" height="100%" fill="red" />
+  <rect x="0" y="0" width="100" height="100" fill='yellow'/>
 
   <circle cx="150" cy="100" r="80" fill="green" />
-
+  <polyline points={x+' 110 65 120 70 115 75 130 80 125 85 140 90 135 95 150 100 145'}
+      stroke="orange" fill="transparent" stroke-width="5"/>
   <text x={x} y="125" fontSize="60" textAnchor="middle" fill="white">SVG</text>
-
+<path d="M20,230 Q40,205 50,230 T90,230" fill="none" stroke="blue" strokeWidth="5"/>
+<path d="M 10 10 h 90 V 90 H 1000 L 10 120 Z" stroke="pink" strokeWidth="5" fill='transparent'/>
 </svg>
 	return <div>hello</div>
 }
