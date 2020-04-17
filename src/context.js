f6e833c7384d8c33a1a79d5926d14a1545575a4c
Yigal Irani <yigal.symbolclick.com>
2020-04-17 18:09:25 -0500

    read write to context from leaf componenets

--- src/context.js
+++ src/context.js
@@ -3,11 +3,15 @@ function Label(){
 	var context=React.useContext(TheContext)
 	return <div style={{background:context.color.val}}>hello context</div>
 }
+function Setter(){
+	var context=React.useContext(TheContext)
+	return <>setter<InputEnter {...{value:context.color}}/></>
+}
 
 function ContextDemo(){
 	var  color=usePair('red')
 	return <TheContext.Provider value={{color}}>
-	     <InputEnter {...{value:color}}/>
+		 <Setter/>
          <Label/>
       </TheContext.Provider>
 }
