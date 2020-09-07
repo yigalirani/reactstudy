987356d50db6b59ee862f4985fcc92e2ef06f5a8
Yigal Irani <yigal@symbolclick.com>
2020-09-07 02:25:15 -0400

    added parent child

---
+++ src/parent_child.js
@@ -0,0 +1,17 @@
+function Child({lookup}){
+    var [count,set_count]=React.useState(0)
+    function diff(){
+        set_count(count+1)
+    }
+    lookup.diff=diff
+    return <div>{count}</div>
+}
+
+function Parent(){
+    var [lookup,set_lookup]=React.useState({})
+    return <div>
+        <Child {...{lookup}}/>
+        <button onClick={_=>lookup.diff(1)}>up</button>
+    </div>
+}
+ReactDOM.render(<Parent />,document.querySelector('#root') );
