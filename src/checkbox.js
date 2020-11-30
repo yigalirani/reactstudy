096d1234c4c1423c7acc8bf7865eb7cd0e7fd207
Yigal Irani <yigal@symbolclick.com>
2020-11-30 16:47:20 -0600

    checkbox

---
+++ src/checkbox.js
@@ -0,0 +1,34 @@
+function Checkbox({checked,set_checked}){
+  function Box({fill="transparent",stroke="black"}){
+    return <path d="M 2 2 L 30 2 L 30 30 L 2 30 z" {...{fill,stroke}} stroke-width="2"/>
+  }
+  function Check(){
+    return <path d="M 6 20 L 12 26 L 26 6" fill="transparent" stroke="white" stroke-width="4"/>
+  }  
+  function Indeterminate(){
+    return <path d="M 6 16 H 24" fill="transparent" stroke="black" stroke-width="2"/>
+  }
+  if (checked==null){
+    return <svg   onClick={_=>set_checked(true)} viewBox="0 0 32 32" height='1em' >
+      <Box/><Indeterminate/>
+    </svg>
+  }
+  if (checked){
+    return<svg  onClick={_=>set_checked(false)} viewBox="0 0 32 32" height='1em' >
+      <Box fill="blue" stroke="transparent"/><Check/>
+    </svg>
+  }
+  return <svg    onClick={_=>set_checked(true)} viewBox="0 0 32 32" height='1em' >
+    <Box/>
+  </svg>
+ }
+
+function CheckboxTest(){
+  var [checked,set_checked]=React.useState(null) 
+  return <div>
+    <Checkbox {...{checked,set_checked}}/>
+    <input type="checkbox" checked/>
+    <input type="checkbox" />
+    <input type="checkbox" checked='checked'/>
+  </div>
+}
