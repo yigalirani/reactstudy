b8dd051994c259a686d2d244728a2e348fce6e01
Yigal Irani <yigal@symbolclick.com>
2021-03-09 21:31:38 -0600

    added State, set_state_from_render

---
+++ src/setstate_from_render.js
@@ -0,0 +1,22 @@
+function Inner({outer_state}){
+  var [inner_state,set_inner_state]=React.useState(outer_state)
+  if (inner_state.length<outer_state.length){ //change < to <= and you got yourself an infiniate loop
+    set_inner_state(outer_state)
+  }
+  return <ol>
+    <li>{inner_state}</li>
+    <li>{outer_state}</li>
+  </ol>
+}
+
+function SetStateFromRender(){
+  var [outer_state,set_outer_state]=React.useState('')
+  function onChange(e){
+		set_outer_state(e.target.value)
+	} 
+	return <div>
+    
+    <input type="text" {...{outer_state,onChange}} />
+    <Inner {...{outer_state}}/> 
+  </div>
+}
---
+++ src/state.js
@@ -0,0 +1,13 @@
+function State(){
+  var [msg,set_msg]=React.useState("hello") //not rendered, just to force render
+  var [data,set_data]=React.useState({version:1})
+  function onClick(){
+    set_msg(x=>x+'x') //just to force render
+    data.version+=1   //changing state in place, but it still updates
+  }
+  return <div>
+    version={data.version}
+    <button {...{onClick}}>click</button>
+  </div>
+}
+ReactDOM.render(<State />,document.querySelector('#root') );
